a3bb539d004350eaf3ebdd14ecf95c259abdc265
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2020-03-02 23:44:13 -0600

    Add identification methods with tests SSN and MBI

--- src/Faker/Identification.cs
+++ src/Faker/Identification.cs
@@ -1,10 +1,70 @@
-﻿using System.Text;
+﻿using System;
+using System.Linq;
+using System.Text;
 using Faker.Extensions;
 
 namespace Faker
 {
     public static class Identification
     {
+        private const string Alpha = "ACDEFGHJKMNPQRTUVWXY";
+        private const string Numeric = "0123456789";
+        private static readonly string AlphaNumeric = string.Join("", Alpha, Numeric);
+        private static readonly string Numeric19 = Numeric.Substring(1);
+
+        /*
+        Position 1 – numeric values 1 thru 9
+        Position 2 – alphabetic values A thru Z (minus S, L, O, I, B, Z)
+        Position 3 – alpha-numeric values 0 thru 9 and A thru Z (minus S, L, O, I, B, Z) Position 4 – numeric values 0 thru 9
+        Position 4 – numeric values 0 thru 9
+        Position 5 – alphabetic values A thru Z (minus S, L, O, I, B, Z)
+        Position 6 – alpha-numeric values 0 thru 9 and A thru Z (minus S, L, O, I, B, Z) Position 7 – numeric values 0 thru 9
+        Position 7 – numeric values 0 thru 9
+        Position 8 – alphabetic values A thru Z (minus S, L, O, I, B, Z)
+        Position 9 – alphabetic values A thru Z (minus S, L, O, I, B, Z)
+        Position 10 – numeric values 0 thru 9
+        Position 11 – numeric values 0 thru 9
+        */
+        // https://www.cms.gov/Medicare/New-Medicare-Card/Understanding-the-MBI-with-Format.pdf
+        public static string MedicareBeneficiaryIdentifier()
+        {
+            return string.Join("", new[]
+            {
+                Numeric19.ElementAt(RandomNumber.Next(0, Numeric19.Length)),
+                Alpha.ElementAt(RandomNumber.Next(0, Alpha.Length)),
+                AlphaNumeric.ElementAt(RandomNumber.Next(0, AlphaNumeric.Length)),
+                Numeric.ElementAt(RandomNumber.Next(0, Numeric.Length)),
+                Alpha.ElementAt(RandomNumber.Next(0, Alpha.Length)),
+                AlphaNumeric.ElementAt(RandomNumber.Next(0, AlphaNumeric.Length)),
+                Numeric.ElementAt(RandomNumber.Next(0, Numeric.Length)),
+                Alpha.ElementAt(RandomNumber.Next(0, Alpha.Length)),
+                Alpha.ElementAt(RandomNumber.Next(0, Alpha.Length)),
+                Numeric.ElementAt(RandomNumber.Next(0, Numeric.Length)),
+                Numeric.ElementAt(RandomNumber.Next(0, Numeric.Length))
+            });
+        }
+
+        public static string SocialSecurityNumber(bool dashFormat = true)
+        {
+            var area = RandomNumber.Next(0, 650);
+
+            /*
+            Odd numbers, 01 to 09
+            Even numbers, 10 to 98
+            Even numbers, 02 to 08
+            Odd numbers, 11 to 99
+            */
+
+            var groups = Enumerable.Range(1, 10).Where(z => z % 2 == 1).ToList();
+            groups.AddRange(Enumerable.Range(10, 99).Where(z => z % 2 == 0));
+            groups.AddRange(Enumerable.Range(2, 9).Where(z => z % 2 == 0));
+            groups.AddRange(Enumerable.Range(11, 99).Where(z => z % 2 == 1));
+            var group = groups.ElementAt(RandomNumber.Next(0, groups.Count));
+            var serial = RandomNumber.Next(1, 10000);
+            var ssn = $"{area:###}-{group:##}-{serial:####}";
+            return !dashFormat ? ssn.Replace("-", "") : ssn;
+        }
+
         private static readonly string[] _alphabet = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".Split(' ');
 
         public static string UKNationalInsuranceNumber()
---
+++ tests/Faker.Tests/IdentificationFixture.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace Faker.Tests
+{
+    [TestFixture]
+    public class IdentificationFixture
+    {
+        [Test]
+        public void Should_Create_SSN()
+        {
+            var ssn = Identification.SocialSecurityNumber();
+            Assert.IsTrue(Regex.IsMatch(ssn, @"\d{3}-\d{2}-\d{4}"));
+        }
+
+        [Test]
+        public void Should_Create_MBI()
+        {
+            var mbi = Identification.MedicareBeneficiaryIdentifier();
+            Assert.IsTrue(Regex.IsMatch(mbi, @"\b[1-9][AC-HJKMNP-RT-Yac-hjkmnp-rt-y][AC-HJKMNP-RT-Yac-hjkmnp-rt-y0-9][0-9]-?[AC-HJKMNP-RT-Yac-hjkmnp-rt-y][AC-HJKMNP-RT-Yac-hjkmnp-rt-y0-9][0-9]-?[AC-HJKMNP-RT-Yac-hjkmnp-rt-y]{2}\d{2}\b"));
+        }
+
+        [Test]
+        public void Should_Create_UKNationalInsuranceNumber()
+        {
+            var nin = Identification.UKNationalInsuranceNumber();
+            Assert.IsTrue(Regex.IsMatch(nin, @"^\s*[a-zA-Z]{2}(?:\s*\d\s*){6}[a-zA-Z]?\s*$"));
+        }
+    }
+}
