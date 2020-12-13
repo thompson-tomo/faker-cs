24e830465ede73298ac0a90baf66f8d959cd81c3
Ollie Riches <ollie_riches@hotmail.com>
2020-12-13 21:46:06 +0000

    Added unit tests for .Net 4.0

--- tests/Faker.Tests.Net.4.0/Properties/AssemblyInfo.cs
+++ tests/Faker.Tests.Net.4.0/Properties/AssemblyInfo.cs
@@ -1,21 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Faker.Net")]
+[assembly: AssemblyTitle("Faker.Tests.Net.4.8")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Faker.Net")]
+[assembly: AssemblyProduct("Faker.Tests.Net.4.8")]
 [assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("6e293261-e726-44bd-bffe-f5c454764512")]
+[assembly: Guid("829bdff1-a65e-456b-b55e-1d1b7043d939")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("Faker.Tests.Net.4.0")]
-[assembly: InternalsVisibleTo("Faker.Tests.Net.4.8")]
