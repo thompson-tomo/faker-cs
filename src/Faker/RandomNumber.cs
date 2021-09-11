94d64207a81dd217977cc22550cb188875b4f45e
Ollie Riches <ollie_riches@hotmail.com>
2021-09-11 20:27:43 +0100

    Fixed RandomNumber class, random number generation not working correctly for boundary values (Int.MinValue, Int.MaxValue)

--- src/Faker/RandomNumber.cs
+++ src/Faker/RandomNumber.cs
@@ -3,17 +3,27 @@ using System.Security.Cryptography;
 
 namespace Faker
 {
+    // constrain our values to between our min and max
+    // https://stackoverflow.com/a/3057867/86411
+    //
     public static class RandomNumber
     {
         private static readonly RandomNumberGenerator Rnd = RandomNumberGenerator.Create();
 
         private static int Next(this RandomNumberGenerator generator, int min, int max)
         {
-            var bytes = new byte[sizeof(int)]; // 4 bytes
+            var bytes = new byte[sizeof(int)];
+            generator.GetNonZeroBytes(bytes);
+            var val = BitConverter.ToUInt32(bytes, 0);
+            var result = ((val - min) % (max - min + 1) + (max - min) + 1) % (max - min + 1) + min;
+            return (int)result;
+        }
+
+        private static long Next(this RandomNumberGenerator generator, long min, long max)
+        {
+            var bytes = new byte[sizeof(long)];
             generator.GetNonZeroBytes(bytes);
             var val = BitConverter.ToInt32(bytes, 0);
-            // constrain our values to between our min and max
-            // https://stackoverflow.com/a/3057867/86411C:\Work\GitHub\faker-cs\src\Faker\RandomNumber.cs
             var result = ((val - min) % (max - min + 1) + (max - min) + 1) % (max - min + 1) + min;
             return result;
         }
@@ -28,7 +38,17 @@ namespace Faker
             return Rnd.Next(0, max);
         }
 
+        public static long Next(long max)
+        {
+            return Rnd.Next(0, max);
+        }
+
         public static int Next(int min, int max)
+        {
+            return Rnd.Next(min, max);
+        }
+
+        public static long Next(long min, long max)
         {
             return Rnd.Next(min, max);
         }
